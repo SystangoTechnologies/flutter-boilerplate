7c10cea75168716505591b9f09fd33fd35c6a0ca
Anurodh Singh <asingh@isystango.com>
2020-08-11 19:42:02 +0530

    updated app name to Flutter Boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:contact_list_demo/ui/home_page.dart';
-import 'package:contact_list_demo/ui/add_contact_page.dart';
-import 'package:contact_list_demo/ui/edit_contact_page.dart';
-import 'package:contact_list_demo/ui/view_contact_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Contact List Demo',
+      title: 'Flutter Boilerplate',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         brightness: Brightness.light
@@ -19,9 +16,6 @@ class MyApp extends StatelessWidget {
       home: HomePage(title: 'Home Page'),
       routes: <String, WidgetBuilder> {
         'Home': (BuildContext context) => HomePage(title: 'Home Page'),
-        'AddContact': (BuildContext context) => AddContactPage(title: 'Add Contact'),
-        'EditContact': (BuildContext context) => EditContactPage(title: 'Edit Contact'),
-        'ViewContact': (BuildContext context) => ViewContactPage(title: 'View Contact'),
       },
     );
   }
